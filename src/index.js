42ce46a221d994e8803d25da783c076f51051987
Areliya13 <sharipovatamara@gmail.com>
2022-07-15 23:53:09 +0300

    feat: add script to index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (arguments.length === 0) {return []}
+  let res = matrix.reduce(function rev(acc, value, index){
+    if (index % 2 === 0) {
+      return [...acc, ...value]
+    } else {
+      return [...acc, ...value.sort((a,b) => b-a)]
+    }
+  },[])
+  return res;
 }
+
+
